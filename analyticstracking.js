1876a7f41955d1b8e6fc7f501181f137d549e233
ludosaas <ludovic.saas@gmail.com>
2013-10-11 20:47:00 +0200

    chnaging analytics code

--- analyticstracking.js
+++ analyticstracking.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-41594070-1', 'ludovicsaas.azurewebsites.net');
+  ga('create', 'UA-41594070-1', 'ludovicsaas.info');
   ga('send', 'pageview');
